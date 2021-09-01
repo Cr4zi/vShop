2a0ea73d42ed5603e4563486106e085fa621e876
ron035994@gmail.com <q3zcmmg8>
2021-09-01 21:17:40 +0300

    add the start signup page

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, async () => {
     console.log("The Server Is Runing On Port 3000, http://localhost:3000/pages/Home.html")
-})
+});
