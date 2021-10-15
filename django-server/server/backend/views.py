90aaf3ca7cdd5ccf0ed831a00aa2a5545ea2317d
Cr4zi <ori.mizrahi1000@gmail.com>
2021-10-15 14:29:18 +0300

    Create index route

--- django-server/server/backend/views.py
+++ django-server/server/backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import json
+
+from .models import *
+
+
+def index(request):
+    res = json.dumps(["It's the index route there is nothing to be here"])
+    return HttpResponse(res, content_type='text/json')
